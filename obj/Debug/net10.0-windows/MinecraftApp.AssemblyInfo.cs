5cff4bd4f19862d311b2d6cb25e718d734f32da6
Nathalia_paih <docyenisnth@gmail.com>
2026-09-14 12:09:36 -0300

    conversor-unidades

--- obj/Debug/net10.0-windows/MinecraftApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MinecraftApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinecraftApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2992b104ead33e301844f706534b9c5162bd6061")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74ce8eda6b9b14ebd7a17d108376b482529043d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinecraftApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinecraftApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
